9c0abcc5f3adebf29da0d4ad226ad870309976a5
dustin <matthewdustin89@gmail.com>
2025-06-01 17:16:25 +0700

    menambah tampilan menu awal dan membuat fungsi setor motor sama nampilin stock

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#define MAX_MOTOR 100
 
 typedef struct {
     char username[30];
@@ -10,10 +11,23 @@ typedef struct {
 User currentUser;
 int isLoggedIn = 0;
 
+typedef struct {
+    char nama[30];
+    int stok;
+    float hargaPerHari;
+} Motor;
+
+Motor daftarMotor[MAX_MOTOR];
+int jumlahMotor = 0;
+
 void clearScreen();
 void tampilanMainMenu();
 void registerUser();
 void loginUser();
+void tampilanMenuMasuk();
+void menuMasuk(int a);
+void tambahMotor();
+void stockMotor();
 
 int main() {
     int pilihan;
@@ -52,6 +66,91 @@ int main() {
     return 0;
 }
 
+void menuMasuk(int a){
+do {
+    clearScreen();
+    tampilanMenuMasuk();
+    printf("Masukkan pilihan: ");
+    scanf("%d", &a);
+    getchar();
+
+    switch (a) {
+        case 1:
+            tambahMotor();
+            break;
+        case 2:
+            stockMotor();
+            break;
+            
+        case 3:
+            printf("menu 3 belum dibuat pak\n");
+            break;
+
+        case 4:
+            printf("menu 4 belum dibuat pak\n");
+            break;
+
+        case 5:
+            printf("menu 5 belum dibuat pak\n");
+            break;
+        case 0:
+            isLoggedIn = 0;
+            printf("Logout berhasil.\n");
+            break;
+        default:
+            printf("Pilihan tidak valid.\n");
+    }
+
+    if (a != 0) {
+        printf("Tekan ENTER untuk kembali...");
+        getchar();
+    }
+} while (a != 0);
+}
+
+void tambahMotor() {
+    if (jumlahMotor >= MAX_MOTOR) {
+        printf("Kapasitas data motor penuh.\n");
+        return;
+    }
+
+    Motor m;
+
+    printf("\n--- TAMBAH DATA MOTOR ---\n");
+
+    printf("Nama Motor: ");
+    fgets(m.nama, sizeof(m.nama), stdin);
+    m.nama[strcspn(m.nama, "\n")] = '\0';
+
+    printf("Jumlah Stok: ");
+    scanf("%d", &m.stok);
+    getchar();
+
+    printf("Harga Sewa per Hari: ");
+    scanf("%f", &m.hargaPerHari);
+    getchar();
+
+    daftarMotor[jumlahMotor] = m;
+    jumlahMotor++;
+
+    printf("Motor berhasil ditambahkan!\n");
+}
+
+void stockMotor() {
+    if (jumlahMotor == 0) {
+        printf("\nBelum ada data motor yang ditambahkan.\n");
+        return;
+    }
+
+    printf("\n--- DAFTAR STOK MOTOR ---\n");
+    for (int i = 0; i < jumlahMotor; i++) {
+        printf("Motor ke-%d\n", i + 1);
+        printf("Nama Motor        : %s\n", daftarMotor[i].nama);
+        printf("Jumlah Stok       : %d\n", daftarMotor[i].stok);
+        printf("Harga per Hari    : Rp %.2f\n", daftarMotor[i].hargaPerHari);
+        printf("-----------------------------------\n");
+    }
+}
 
 void clearScreen() {
     #ifdef _WIN32
@@ -66,6 +165,20 @@ void tampilanMainMenu(){
     printf("=   	          1.REGISTER                    =\n");
     printf("=                 2.LOGIN                       =\n");
     printf("=                 0.EXIT                        =\n");
+    printf("=================================================\n");
+    printf("= Dari Kami, Untuk Perjalanan Tak Terlupakanmu. =\n");
+    printf("=================================================\n");
+}
+void tampilanMenuMasuk(){
+    printf("=================================================\n");
+    printf("=    SELAMAT DATANG DI SEWA MOTOR SATUHATI      =\n");
+    printf("=================================================\n");
+    printf("=   	      1.Tambah Stok Motor               =\n");
+    printf("=             2.Semua Jenis Motor               =\n");
+    printf("=             3.Tambah Data Penyewa             =\n");
+    printf("=             4.Kalkulasi Harga                 =\n");
+    printf("=             5.Cetak Struk                     =\n");
+    printf("=             0.LOGOUT                          =\n");
     printf("=================================================\n");
     printf("= Dari Kami, Untuk Perjalanan Tak Terlupakanmu. =\n");
     printf("=================================================\n");
@@ -120,6 +233,7 @@ void loginUser() {
 
 
         printf("\nSelamat datang Sang Atmin!.\n");
+        menuMasuk(0);
 
     } else {
         printf("Username atau password salah. Coba lagi.\n");
